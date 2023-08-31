8191bec20d2b2083cb7eb266483a61a1fc4cc6a3
Akash Khan <akashcc48@gmail.com>
2023-08-31 20:39:02 +0600

    categories tab showed

--- js/index.js
+++ js/index.js
@@ -1,1 +1,30 @@
+const loadCategories = async () => {
+  const res = await fetch(
+    "https://openapi.programming-hero.com/api/videos/categories"
+  );
+  const data = await res.json();
+  const categories = data.data;
 
+  const tabContainer = document.getElementById("tab-container");
+  categories.forEach((category) => {
+    const div = document.createElement("div");
+    div.innerHTML = `
+    
+    <a onclick="handleCategoryData('${category.category_id}')" class="tab">${category.category}</a>
+
+    
+    `;
+    tabContainer.appendChild(div);
+  });
+};
+
+const handleCategoryData = async (id) => {
+  const res = await fetch(
+    `https://openapi.programming-hero.com/api/videos/category/${id}`
+  );
+  const data = await res.json();
+  const category = data.data;
+  console.log(category);
+};
+
+loadCategories();
